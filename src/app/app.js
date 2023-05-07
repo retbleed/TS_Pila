739087cea7d85e54dea737de92a63d79b5f923a8
alexander <hsa.lapaz@gmail.com>
2023-05-07 11:52:33 -0600

    update A1
    La pila esta funcionando al 100% falta arreglar el diseño de la página para que sea mas agradable a la vista.

--- src/app/app.js
+++ src/app/app.js
@@ -1,22 +1,85 @@
 document.addEventListener("DOMContentLoaded", function () {
     var container = document.querySelector(".stackContainer");
-    var inputText = document.getElementById('textHolder');
+    var inputText = document.getElementById("textHolder");
     var variable = 0;
-    function addElementToStack() {
+    var array = [];
+    function pushElement() {
         if (variable <= 7 && inputText.value != "") {
             var newElement = document.createElement("div");
             var textA = inputText.value;
             newElement.classList.add("stackElement");
             var text = document.createTextNode(textA);
+            array.push(inputText.value);
             newElement.appendChild(text);
             container.appendChild(newElement);
+            variable += 1;
+            inputText.value = "";
+            if (container) {
+                var lastBox_1 = container.lastChild;
+                setTimeout(function () {
+                    lastBox_1.style.backgroundColor = "rgb(221, 221, 221)";
+                }, 2000); // Cambio de color después de 2 segundos
+            }
         }
         else {
             alert("Ha ocurrido un error inesperado");
         }
     }
+    function popElement() {
+        var lastElement = container.lastChild;
+        if (lastElement) {
+            setTimeout(function () {
+                lastElement.style.backgroundColor = "rgb(255, 68, 68)";
+            }, 2000); // Cambio de color después de 2 segundos
+            setTimeout(function () {
+                alert("El ultimo valor fue: " + lastElement.textContent);
+                container.removeChild(lastElement);
+                array.pop();
+            }, 4000);
+        }
+        inputText.value = "";
+    }
+    function peekElement() {
+        var lastElement = container.lastChild;
+        if (lastElement) {
+            alert("El ultimo valor fue: " + lastElement.textContent);
+        }
+        inputText.value = "";
+    }
+    function sizeElement() {
+        alert("El tamaño de la pila es de: " + array.length);
+    }
     var button = document.querySelector(".optionButton");
     button.addEventListener("click", function () {
-        addElementToStack();
+        var options = document.querySelectorAll('input[type="radio"][name="option"]');
+        var selectedOption;
+        for (var i = 0; i < options.length; i++) {
+            var option = options[i];
+            if (option.checked) {
+                selectedOption = option;
+                break;
+            }
+        }
+        if (selectedOption) {
+            switch (selectedOption.value) {
+                case "push":
+                    pushElement();
+                    return;
+                case "pop":
+                    popElement();
+                    return;
+                case "peek":
+                    peekElement();
+                    return;
+                case "size":
+                    sizeElement();
+                    return;
+                default:
+                    return;
+            }
+        }
+        else {
+            alert("Elige una opcion!");
+        }
     });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,93 @@
 document.addEventListener("DOMContentLoaded", function () {
   const container = document.querySelector(".stackContainer") as HTMLElement;
-  const inputText = document.getElementById('textHolder') as HTMLInputElement;
+  const inputText = document.getElementById("textHolder") as HTMLInputElement;
   let variable = 0;
-  function addElementToStack() {
-    if(variable <= 7 && inputText.value != ""){
-        const newElement = document.createElement("div");
-        const textA = inputText.value;
-        newElement.classList.add("stackElement");
-        const text = document.createTextNode(textA);
-        newElement.appendChild(text);
-        container.appendChild(newElement);
-    }else{
-        alert("Ha ocurrido un error inesperado");
+  let array: string[] = [];
+
+  function pushElement() {
+    if (variable <= 7 && inputText.value != "") {
+      const newElement = document.createElement("div");
+      const textA = inputText.value;
+      newElement.classList.add("stackElement");
+      const text = document.createTextNode(textA);
+      array.push(inputText.value);
+      newElement.appendChild(text);
+      container.appendChild(newElement);
+      variable += 1;
+      inputText.value = "";
+
+      if (container) {
+        const lastBox = container.lastChild as HTMLElement;
+        setTimeout(() => {
+          lastBox.style.backgroundColor = "rgb(221, 221, 221)";
+        }, 2000); // Cambio de color después de 2 segundos
+      }
+    } else {
+      alert("Ha ocurrido un error inesperado");
     }
+  }
+
+  function popElement() {
+    const lastElement = container.lastChild as HTMLElement;
+    if (lastElement) {
+      setTimeout(() => {
+        lastElement.style.backgroundColor = "rgb(255, 68, 68)";
+      }, 2000); // Cambio de color después de 2 segundos
+      setTimeout(() => {
+        alert("El ultimo valor fue: " + lastElement.textContent);
+        container.removeChild(lastElement);
+        array.pop();
+      }, 4000);
+    }
+    inputText.value = "";
+  }
+
+  function peekElement(){
+    const lastElement = container.lastChild as HTMLElement;
+    if (lastElement) {
+      alert("El ultimo valor fue: " + lastElement.textContent);
+    }
+    inputText.value = "";    
+  }
+
+  function sizeElement(){
+    alert("El tamaño de la pila es de: " + array.length);
   }
 
   const button = document.querySelector(".optionButton") as HTMLElement;
   button.addEventListener("click", () => {
-    addElementToStack();
+    const options = document.querySelectorAll<HTMLInputElement>(
+      'input[type="radio"][name="option"]'
+    );
+    let selectedOption;
+
+    for (let i = 0; i < options.length; i++) {
+      const option = options[i];
+      if (option.checked) {
+        selectedOption = option;
+        break;
+      }
+    }
+
+    if (selectedOption) {
+      switch (selectedOption.value) {
+        case "push":
+          pushElement();
+          return;
+        case "pop":
+          popElement();
+          return;
+        case "peek":
+          peekElement();
+          return;
+        case "size":
+          sizeElement();
+          return;
+        default:
+          return;
+      }
+    } else {
+      alert("Elige una opcion!");
+    }
   });
 });
